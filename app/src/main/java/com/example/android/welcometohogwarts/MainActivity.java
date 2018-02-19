2060a9976ed404b67ad3f796d2f65513a88d8000
Inoarefst <33956323+Inoarefst@users.noreply.github.com>
2018-02-19 10:23:14 +0200

    Adding fields

--- app/src/main/java/com/example/android/welcometohogwarts/MainActivity.java
+++ app/src/main/java/com/example/android/welcometohogwarts/MainActivity.java
@@ -16,32 +16,31 @@ import android.widget.Toast;
  */
 
 public class MainActivity extends AppCompatActivity {
-    RadioButton AnswerQuestion1;
+    RadioButton AnswerQuestion6;
     RadioButton AnswerQuestion2;
     RadioButton AnswerQuestion3;
     RadioButton AnswerQuestion4;
     RadioButton AnswerQuestion5;
-    RadioButton AnswerQuestion6;
     RadioButton AnswerQuestion7;
     RadioButton AnswerQuestion8;
     CheckBox AnswerQuestion9;
+    CheckBox CorrectQuestion9;
     RadioGroup radioWhich;
     RadioGroup radioCommonRoom;
     RadioGroup radioEmblematic;
     RadioGroup radioElement;
     RadioGroup radioAbilities;
-    RadioGroup radioStudent;
     RadioGroup radioEntrance;
     RadioGroup radioIgnatia;
     CheckBox checkBoxTom;
     EditText name;
+    EditText year;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AnswerQuestion1 = findViewById(R.id.question1_answer);
         AnswerQuestion2 = findViewById(R.id.question2_answer);
         AnswerQuestion3 = findViewById(R.id.question3_answer);
         AnswerQuestion4 = findViewById(R.id.question4_answer);
@@ -50,16 +49,17 @@ public class MainActivity extends AppCompatActivity {
         AnswerQuestion7 = findViewById(R.id.question7_answer);
         AnswerQuestion8 = findViewById(R.id.question8_answer);
         AnswerQuestion9 = findViewById(R.id.question9_answer);
+        CorrectQuestion9 = findViewById(R.id.question9_correct);
         radioWhich = findViewById(R.id.radio_group_which);
         radioCommonRoom = findViewById(R.id.radio_group_common_room);
         radioEmblematic = findViewById(R.id.radio_group_emblematic);
         radioElement = findViewById(R.id.radio_group_element);
         radioAbilities = findViewById(R.id.radio_group_abilities);
-        radioStudent = findViewById(R.id.radio_group_hufflepuff_student);
         radioEntrance = findViewById(R.id.radio_group_entrance);
         radioIgnatia = findViewById(R.id.radio_group_ignatia);
         checkBoxTom = findViewById(R.id.question9_false);
         name = findViewById(R.id.users_name_view);
+        year = findViewById(R.id.year);
     }
 
     /**
@@ -68,96 +68,58 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void showScore(View view) {
-
+        int score = 0;
         //Get user's name.
         EditText usersName = findViewById(R.id.users_name_view);
         String name = usersName.getText().toString();
 
-        boolean hasAnswerQuestion1 = AnswerQuestion1.isChecked();
+        EditText Riddle = findViewById(R.id.year);
+        String year = Riddle.getText().toString();
+        if(year.equals("1926")) score+=5;
 
         boolean hasAnswerQuestion2 = AnswerQuestion2.isChecked();
+        if(hasAnswerQuestion2) score+=5;
 
         boolean hasAnswerQuestion3 = AnswerQuestion3.isChecked();
+        if(hasAnswerQuestion3) score+=5;
 
         boolean hasAnswerQuestion4 = AnswerQuestion4.isChecked();
+        if(hasAnswerQuestion4) score+=5;
 
         boolean hasAnswerQuestion5 = AnswerQuestion5.isChecked();
+        if(hasAnswerQuestion5) score+=5;
 
         boolean hasAnswerQuestion6 = AnswerQuestion6.isChecked();
+        if(hasAnswerQuestion6) score = score + 5;
 
         boolean hasAnswerQuestion7 = AnswerQuestion7.isChecked();
+        if(hasAnswerQuestion7) score+=5;
 
         boolean hasAnswerQuestion8 = AnswerQuestion8.isChecked();
+        if(hasAnswerQuestion8) score+=5;
 
         boolean hasAnswerQuestion9 = AnswerQuestion9.isChecked();
-
+        boolean hasCorrectQuestion9 = CorrectQuestion9.isChecked();
+        boolean hasCheckBoxTom = checkBoxTom.isChecked();
+        if((hasAnswerQuestion9 && hasCorrectQuestion9)) score+=10;
+        if((hasAnswerQuestion9 && hasCheckBoxTom) ||(hasCorrectQuestion9 && hasCheckBoxTom) ) score+=5;
+        if((hasAnswerQuestion9 && hasCheckBoxTom && hasCorrectQuestion9))  score-=15;
 
         MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.themesong);
         mediaPlayer.start();
 
-        int score = numberOfCorrectAnswers(hasAnswerQuestion1, hasAnswerQuestion2, hasAnswerQuestion3, hasAnswerQuestion4, hasAnswerQuestion5, hasAnswerQuestion6, hasAnswerQuestion7, hasAnswerQuestion8, hasAnswerQuestion9) * 5;
-
-        if (score == 45) {
-            Toast.makeText(this, "Perfect " + name + "\n Your score is " + score + " out of 45 " + "\n You should be Minister of Magic !", Toast.LENGTH_LONG).show();
+        if (score == 50) {
+            Toast.makeText(this, " Perfect " + name + "\n Your score is " + score + " out of 50 " + "\n You should be Minister of Magic !", Toast.LENGTH_LONG).show();
         }
 
-        if ((score <= 40) && (score > 25)) {
-            Toast.makeText(this, name + "\n your score is " + score + " out of 45 " + "\n You are going to be an excellent Wizard", Toast.LENGTH_LONG).show();
+        if ((score <= 45) && (score > 25)) {
+            Toast.makeText(this,  name + "\n your score is " + score + " out of 50 " + "\n You are going to be an excellent Wizard", Toast.LENGTH_LONG).show();
         }
         if (score == 25) {
-            Toast.makeText(this, name + "\n your score is " + score + " out of 45 " + "\n You have great skills . Keep up the good work .", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,  name + "\n your score is " + score + " out of 50 " + "\n You have great skills . Keep up the good work .", Toast.LENGTH_LONG).show();
         } else if (score < 25) {
-            Toast.makeText(this, name + "\n your score is " + score + " out of 45 " + "\n You must try harder . Muggles can score better than this!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,  name + "\n your score is " + score + " out of 50 " + "\n You must try harder . Muggles can score better than this!", Toast.LENGTH_LONG).show();
         }
-
-    }
-
-
-    /**
-     * This method is used to count the number of correct answers of the user.
-     *
-     * @param AnswerQuestion1 is whether or not the user has answered question1 correctly.
-     * @param AnswerQuestion2 is whether or not the user has answered question2 correctly.
-     * @param AnswerQuestion3 is whether or not the user has answered question3 correctly.
-     * @param AnswerQuestion4 is whether or not the user has answered question4 correctly.
-     * @param AnswerQuestion5 is whether or not the user has answered question5 correctly.
-     * @param AnswerQuestion6 is whether or not the user has answered question6 correctly.
-     * @param AnswerQuestion7 is whether or not the user has answered question7 correctly.
-     * @param AnswerQuestion8 is whether or not the user has answered question8 correctly.
-     * @param AnswerQuestion9 is whether or not the user has answered question9 correctly.
-     * @return number of correct answers .
-     */
-    private int numberOfCorrectAnswers(boolean AnswerQuestion1, boolean AnswerQuestion2, boolean AnswerQuestion3, boolean AnswerQuestion4, boolean AnswerQuestion5, boolean AnswerQuestion6, boolean AnswerQuestion7, boolean AnswerQuestion8, boolean AnswerQuestion9) {
-        int numberOfCorrectAnswers = 0;
-
-        if (AnswerQuestion1) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion2) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion3) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion4) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion5) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion6) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion7) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion8) {
-            numberOfCorrectAnswers += 1;
-        }
-        if (AnswerQuestion9) {
-            numberOfCorrectAnswers += 1;
-        }
-        return numberOfCorrectAnswers;
     }
 
     /**
@@ -165,18 +127,19 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the restart button is clicked.
      */
     public void Reset(View view) {
-        radioWhich.clearCheck();
+        name.setText("");
+        year.setText("");
         radioCommonRoom.clearCheck();
         radioEmblematic.clearCheck();
         radioElement.clearCheck();
         radioAbilities.clearCheck();
-        radioStudent.clearCheck();
+        radioWhich.clearCheck();
         radioEntrance.clearCheck();
         radioIgnatia.clearCheck();
         checkBoxTom.setChecked(false);
         AnswerQuestion9.setChecked(false);
-        name.setText("");
+        CorrectQuestion9.setChecked(false);
+
 
     }
-
 }
